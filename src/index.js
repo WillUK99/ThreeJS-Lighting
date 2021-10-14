4102b088f51105f88a9c4e22da8c778066221464
WillUK99 <willuk1999@gmail.com>
2021-10-14 20:59:36 +0100

    Boilerplate code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import './style/main.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+
 
 /**
- * Sizes
+ * Sizes 
  */
 const sizes = {}
 sizes.width = window.innerWidth
 sizes.height = window.innerHeight
 
-window.addEventListener('resize', () =>
-{
-    // Save sizes
+window.addEventListener("resize", () => {
+
+    // Save sizes on resize
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
@@ -22,40 +25,134 @@ window.addEventListener('resize', () =>
     renderer.setSize(sizes.width, sizes.height)
 })
 
+
 /**
- * Environnements
+ * Environment
  */
-// Scene
+//Scene
 const scene = new THREE.Scene()
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+// Camera 
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0, 10, 20)
 scene.add(camera)
 
-// Test
-const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
-scene.add(cube)
+
+// Orbit Controls
+const controls = new OrbitControls(camera, document.querySelector(".webgl"))
+controls.target.set(0,5,0)
+controls.update()
+
+
+// Plane Texture
+const planeSize = 40
+
+const loader = new THREE.TextureLoader() 
+const texture = loader.load("../assets/checkerboard.png")
+texture.wrapS = THREE.RepeatWrapping
+texture.wrapT = THREE.RepeatWrapping
+texture.magFilter = THREE.NearestFilter
+const repeats = planeSize / 2
+texture.repeat.set(repeats, repeats)
+
+
+// Plane
+const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize)
+const planeMat = new THREE.MeshPhongMaterial({
+    map: texture,
+    side: THREE.DoubleSide,
+})
+
+const planeMesh = new THREE.Mesh(planeGeo, planeMat)
+planeMesh.rotation.x = Math.PI * -0.5
+scene.add(planeMesh)
+
+// Cube
+
+
+// Sphere
+
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('.webgl')
+    canvas: document.querySelector(".webgl")
 })
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(sizes.width, sizes.height)
 
 /**
- * Loop
+ * Animation loop
  */
-const loop = () =>
-{
-    // Update
-    cube.rotation.y += 0.01
+const main = (time) => {
+    // Ms to seconds
+    time *= 0.001
 
-    // Render
+    // Renderer needs to render these
     renderer.render(scene, camera)
 
-    // Keep looping
-    window.requestAnimationFrame(loop)
+    // Create infinite loop
+    window.requestAnimationFrame(main)
 }
-loop()
+
+main()
+
+
+// /**
+//  * Sizes
+//  */
+// const sizes = {}
+// sizes.width = window.innerWidth
+// sizes.height = window.innerHeight
+
+// window.addEventListener('resize', () =>
+// {
+//     // Save sizes
+//     sizes.width = window.innerWidth
+//     sizes.height = window.innerHeight
+
+//     // Update camera
+//     camera.aspect = sizes.width / sizes.height
+//     camera.updateProjectionMatrix()
+
+//     // Update renderer
+//     renderer.setSize(sizes.width, sizes.height)
+// })
+
+// /**
+//  * Environnements
+//  */
+// // Scene
+// const scene = new THREE.Scene()
+
+// // Camera
+// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// camera.position.z = 5
+// scene.add(camera)
+
+// // Test
+// const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
+// scene.add(cube)
+
+// // Renderer
+// const renderer = new THREE.WebGLRenderer({
+//     canvas: document.querySelector('.webgl')
+// })
+// renderer.setPixelRatio(window.devicePixelRatio)
+// renderer.setSize(sizes.width, sizes.height)
+
+// /**
+//  * Loop
+//  */
+// const loop = () =>
+// {
+//     // Update
+//     cube.rotation.y += 0.01
+//     cube.rotation.x += 0.01
+
+//     // Render
+//     renderer.render(scene, camera)
+
+//     // Keep looping
+//     window.requestAnimationFrame(loop)
+// }
+// loop()
