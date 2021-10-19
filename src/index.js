c75ae943829746b17b36c2c2cb3c228fa6a233e0
WillUK99 <willuk1999@gmail.com>
2021-10-19 20:16:39 +0100

    Playing around with 3js lighting and lighting helpers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style/main.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GUI } from 'dat.gui'
 
+console.log("https://threejsfundamentals.org/threejs/lessons/threejs-lights.html")
 
 
 /**
@@ -31,6 +33,7 @@ window.addEventListener("resize", () => {
  */
 //Scene
 const scene = new THREE.Scene()
+scene.background = new THREE.Color("black")
 
 // Camera 
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
@@ -40,15 +43,15 @@ scene.add(camera)
 
 // Orbit Controls
 const controls = new OrbitControls(camera, document.querySelector(".webgl"))
-controls.target.set(0,5,0)
+controls.target.set(0, 5, 0)
 controls.update()
 
 
 // Plane Texture
-const planeSize = 40
+const planeSize = 256
 
-const loader = new THREE.TextureLoader() 
-const texture = loader.load("../assets/checkerboard.png")
+const loader = new THREE.TextureLoader()
+const texture = loader.load('https://threejsfundamentals.org/threejs/resources/images/checker.png')
 texture.wrapS = THREE.RepeatWrapping
 texture.wrapT = THREE.RepeatWrapping
 texture.magFilter = THREE.NearestFilter
@@ -69,8 +72,26 @@ scene.add(planeMesh)
 
 // Cube
 
+const cubeSize = 4
+const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize)
+const cubeMat = new THREE.MeshPhongMaterial({ color: 0xf6f6 })
+const cubeMesh = new THREE.Mesh(cubeGeo, cubeMat)
+cubeMesh.position.set(cubeSize + 1, cubeSize, 0)
+scene.add(cubeMesh)
+
+
 
 // Sphere
+{
+    const sphereR = 3
+    const sphereWidthDivisions = 32
+    const sphereHeightDivisions = 16
+    const sphereGeo = new THREE.SphereGeometry(sphereR, sphereWidthDivisions, sphereHeightDivisions)
+    const sphereMat = new THREE.MeshPhongMaterial({ color: "#CA8" })
+    const mesh = new THREE.Mesh(sphereGeo, sphereMat)
+    mesh.position.set(-sphereR - 1, sphereR + 2, 0)
+    scene.add(mesh)
+}
 
 
 // Renderer
@@ -80,12 +101,85 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(sizes.width, sizes.height)
 
+
+/**
+ * Lighting
+ */
+
+// Point Light
+const pointLight = new THREE.PointLight(0xBA7FFF, 1)
+pointLight.position.set(0, 5, 0)
+scene.add(pointLight)
+const pointLightHelper = new THREE.PointLightHelper(pointLight) // Dont forget to pass the helper the light you wish it to "help"
+scene.add(pointLightHelper)
+
+// Lighting updating
+const updateLight = () => {
+    // If the Light be using doesnt have/need a target then you dont need below
+    // dirLight.target.updateMatrixWorld() 
+    pointLightHelper.update()
+}
+updateLight()
+
+
+/**
+ * GUI
+ */
+// Color
+class ColorGUIHelper {
+    constructor(object, prop) {
+        this.object = object
+        this.prop = prop
+    }
+    get value() {
+        // Getting the color property/key from light object
+        return `#${this.object[this.prop].getHexString()}`
+    }
+    set value(hexString) {
+        // Setting the color propety/key for the light object
+        this.object[this.prop].set(hexString)
+    }
+}
+
+// Creating GUI
+const gui = new GUI()
+// Adding new color controller to GUI - Point Light
+gui.addColor(new ColorGUIHelper(pointLight, "color"), "value").name("color")
+gui.add(pointLight, "intensity", 0, 2, 0.01)
+gui.add(pointLight, "distance", 0, 40).onChange(updateLight)
+
+
+
+
+// Function to allow us to make a controller folder within dat.gui
+const makeXYZGUI = (gui, vector3, name, onChangeFn) => {
+    const folder = gui.addFolder(name)
+    folder.add(vector3, "x", -10, 10).onChange(onChangeFn)
+    folder.add(vector3, "y", 0, 10).onChange(onChangeFn)
+    folder.add(vector3, "z", -10, 10).onChange(onChangeFn)
+    folder.open()
+}
+
+
+
+// makeXYZGUI(gui, dirLight.position, "position", updateLight)
+// makeXYZGUI(gui, dirLight.target.position, "target", updateLight)
+
+makeXYZGUI(gui, pointLight.position, "position", updateLight)
+
+
+
 /**
  * Animation loop
  */
+
+let distance = 0.05
 const main = (time) => {
     // Ms to seconds
     time *= 0.001
+
+    cubeMesh.rotation.x = time / 2
+    cubeMesh.rotation.y = time / 2
 
     // Renderer needs to render these
     renderer.render(scene, camera)
@@ -95,64 +189,3 @@ const main = (time) => {
 }
 
 main()
-
-
-// /**
-//  * Sizes
-//  */
-// const sizes = {}
-// sizes.width = window.innerWidth
-// sizes.height = window.innerHeight
-
-// window.addEventListener('resize', () =>
-// {
-//     // Save sizes
-//     sizes.width = window.innerWidth
-//     sizes.height = window.innerHeight
-
-//     // Update camera
-//     camera.aspect = sizes.width / sizes.height
-//     camera.updateProjectionMatrix()
-
-//     // Update renderer
-//     renderer.setSize(sizes.width, sizes.height)
-// })
-
-// /**
-//  * Environnements
-//  */
-// // Scene
-// const scene = new THREE.Scene()
-
-// // Camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-// camera.position.z = 5
-// scene.add(camera)
-
-// // Test
-// const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
-// scene.add(cube)
-
-// // Renderer
-// const renderer = new THREE.WebGLRenderer({
-//     canvas: document.querySelector('.webgl')
-// })
-// renderer.setPixelRatio(window.devicePixelRatio)
-// renderer.setSize(sizes.width, sizes.height)
-
-// /**
-//  * Loop
-//  */
-// const loop = () =>
-// {
-//     // Update
-//     cube.rotation.y += 0.01
-//     cube.rotation.x += 0.01
-
-//     // Render
-//     renderer.render(scene, camera)
-
-//     // Keep looping
-//     window.requestAnimationFrame(loop)
-// }
-// loop()
